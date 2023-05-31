2806577020f85511b76cb3454ad9fec99472ddb2
Vasco Correia <vasco.correia@rnl.tecnico.ulisboa.pt>
2023-05-31 03:18:22 +0100

    Move Worker to gradle

--- worker/run.sh
+++ worker/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "Go to https://grupos.ist.utl.pt/meic-cnv/project/index-22-23.html to send requests"
-java -cp webserver/target/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar -javaagent:webserver/target/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar=Metrics:pt.ulisboa.tecnico.cnv:output pt.ulisboa.tecnico.cnv.webserver.WebServer
+java -cp webserver/build/libs/webserver.jar -javaagent:webserver/build/libs/webserver.jar=Metrics:pt.ulisboa.tecnico.cnv:output pt.ulisboa.tecnico.cnv.webserver.WebServer
+
