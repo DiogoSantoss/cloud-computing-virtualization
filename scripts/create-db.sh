3d1eba75e87636abf180bc4ffe9e487d8dfeaa20
Vasco Correia <vasco.correia@rnl.tecnico.ulisboa.pt>
2023-06-05 00:19:32 +0100

    Add script to create the dynamo-db table

---
+++ scripts/create-db.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Delete table if already exists
+echo "Deleting any pre-existing table with the same name"
+aws dynamodb delete-table \
+  --region $AWS_DEFAULT_REGION \
+  --table-name $DYNAMO_DB_TABLE_NAME 2>> /dev/null
+
+echo "Creating the table"
+aws dynamodb create-table \
+  --region $AWS_DEFAULT_REGION \
+  --table-name $DYNAMO_DB_TABLE_NAME \
+  --attribute-definition \
+    AttributeName=RequestParams,AttributeType=S \
+  --key-schema \
+    AttributeName=RequestParams,KeyType=HASH \
+  --provisioned-throughput \
+    ReadCapacityUnits=1,WriteCapacityUnits=1 \
+  --table-class STANDARD 2>&1 | jq .
+
+echo "Waiting until table is available"
+aws dynamodb wait table-exists \
+  --region $AWS_DEFAULT_REGION \
+  --table-name $DYNAMO_DB_TABLE_NAME
+
+# echo "Testing Writing to the Table"
+# aws dynamodb put-item \
+#     --region $AWS_DEFAULT_REGION \
+#     --table-name $DYNAMO_DB_TABLE_NAME  \
+#     --item \
+#         '{"RequestParams": {"S": "random_request"}, "BasicBlockCount": {"N": "10"}, "InstCount": {"N": "11"}}'
+# 
+# echo "Testing Reading from the Table"
+# aws dynamodb get-item \
+#   --region $AWS_DEFAULT_REGION \
+#   --table-name $DYNAMO_DB_TABLE_NAME \
+#   --key '{"RequestParams": {"S": "random_request"}}'
+
