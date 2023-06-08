af94036538a79ee2f737d49ffa4ad7ea7795f93d
Diogo Santos <diogosantoss360@gmail.com>
2023-06-08 14:48:43 +0100

    Metrics for foxandrabbit

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -41,7 +41,7 @@ def sendAllRequests():
 
     # range [min, max]
     simulate_args = [
-        [1, 100, 10],  # generations,
+        [1, 1000, 50],  # generations,
         [1, 5, 1],  # world, NOTA VAI ATE 4 MAS MAIS 1 NO RANGE
         [1, 4, 1]  # scenario NOTA VAI ATE 4 MAS MAIS 1 NO RANGE
     ]
@@ -64,13 +64,13 @@ def sendAllRequests():
     #     print(war_endpoint)
     #     requests.get(war_endpoint)
 
-    for i in range(war_args[0][0], war_args[0][1], war_args[0][2]):  # max
-        for j in range(war_args[1][0], war_args[1][1], war_args[1][2]):  # army1    
-            for k in range(war_args[2][0], war_args[2][1], war_args[2][2]):  # army2
-                war_endpoint = "http://localhost:8000/insectwar?max={}&army1={}&army2={}".format(
-                    i, j, k)
-                print(war_endpoint)
-                requests.get(war_endpoint)
+    # for i in range(war_args[0][0], war_args[0][1], war_args[0][2]):  # max
+    #     for j in range(war_args[1][0], war_args[1][1], war_args[1][2]):  # army1    
+    #         for k in range(war_args[2][0], war_args[2][1], war_args[2][2]):  # army2
+    #             war_endpoint = "http://localhost:8000/insectwar?max={}&army1={}&army2={}".format(
+    #                 i, j, k)
+    #             print(war_endpoint)
+    #             requests.get(war_endpoint)
 
     # for j in range(war_args[1][0], war_args[1][1], war_args[1][2]):  # army1
     #     for i in range(war_args[0][0], war_args[0][1], war_args[0][2]):  # max
@@ -110,16 +110,13 @@ def sendAllRequests():
     #         k)
     #     requests.get(simulate_endpoint)
 
-    # for k in range(simulate_args[2][0], simulate_args[2][1], simulate_args[2][2]):  # scenario
-    #     # generations
-    #     for i in range(simulate_args[0][0], simulate_args[0][1], simulate_args[0][2]):
-    #         # world
-    #         for j in range(simulate_args[1][0], simulate_args[1][1], simulate_args[1][2]):
-    #             simulate_endpoint = "http://localhost:8000/simulate?generations={}&world={}&scenario={}".format(
-    #                 i, j, k)
-    #             requests.get(simulate_endpoint)
-    #         break
-    #     break
+    for i in range(simulate_args[0][0], simulate_args[0][1], simulate_args[0][2]): # generations
+        for k in range(simulate_args[2][0], simulate_args[2][1], simulate_args[2][2]):  # scenario    
+            for j in range(simulate_args[1][0], simulate_args[1][1], simulate_args[1][2]): # world
+                simulate_endpoint = "http://localhost:8000/simulate?generations={}&world={}&scenario={}".format(i, j, k)
+                print(simulate_endpoint)
+                requests.get(simulate_endpoint)
+   
 
 
 def sendImages():
@@ -188,21 +185,21 @@ if __name__ == "__main__":
                             instructions.append(int(row[5]))
 
             # the line below are about exponential stuff
-            arr = np.polyfit(x, np.log(instructions),1, w=np.sqrt(instructions))
-            slope, intercept = arr
-            equation = "y={:.3e}+exp({:.3e}x)".format(intercept,slope)
-            print(arr)
+            #arr = np.polyfit(x, np.log(instructions),1, w=np.sqrt(instructions))
+            #slope, intercept = arr
+            #equation = "y={:.3e}+exp({:.3e}x)".format(intercept,slope)
+            #print(arr)
 
-            def exponential_2(x, a, b, c):
-                return a * (x**(b*2)) + c
+            #def exponential_2(x, a, b, c):
+            #    return a * (x**(b*2)) + c
             
-            popt, pcov = curve_fit(exponential_2, x, instructions)
-            print(popt)
+            #popt, pcov = curve_fit(exponential_2, x, instructions)
+            #print(popt)
             #print(pcov)
-            equation = "y={:.5e}*(x**({:.5e}*2))+{:.5e}".format(popt[0],popt[1], popt[2])
+            #equation = "y={:.5e}*(x**({:.5e}*2))+{:.5e}".format(popt[0],popt[1], popt[2])
 
-            plot.set_title(equation)
-            #plot.set_title(compute_slope_equation(x, instructions))
+            #plot.set_title(equation)
+            plot.set_title(compute_slope_equation(x, instructions))
             plot.set_xlabel(x_label)
             plot.plot(x, basicBlocks, label='Basic Blocks')
             plot.plot(x, instructions, label='Instructions')
@@ -227,16 +224,36 @@ if __name__ == "__main__":
             plot.scatter(x, basicBlocks, label='Basic Blocks')
             plot.scatter(x, instructions, label='Instructions')
 
-        SIZE = (3, 3)
+        SIZE = (3, 4)
+
+        # Multiple generations for all scenarios of world 1
+        draw_plot(0,0,"simulate", 1, "Generations", lambda row: int(row[2]) == 1 and int(row[3]) == 1)
+        draw_plot(1,0,"simulate", 1, "Generations", lambda row: int(row[2]) == 1 and int(row[3]) == 2)
+        draw_plot(2,0,"simulate", 1, "Generations", lambda row: int(row[2]) == 1 and int(row[3]) == 3)
+
+        # Multiple generations for all scenarios of world 2
+        draw_plot(0,1,"simulate", 1, "Generations", lambda row: int(row[2]) == 2 and int(row[3]) == 1)
+        draw_plot(1,1,"simulate", 1, "Generations", lambda row: int(row[2]) == 2 and int(row[3]) == 2)
+        draw_plot(2,1,"simulate", 1, "Generations", lambda row: int(row[2]) == 2 and int(row[3]) == 3)
+
+        # Multiple generations for all scenarios of world 3
+        draw_plot(0,2,"simulate", 1, "Generations", lambda row: int(row[2]) == 3 and int(row[3]) == 1)
+        draw_plot(1,2,"simulate", 1, "Generations", lambda row: int(row[2]) == 3 and int(row[3]) == 2)
+        draw_plot(2,2,"simulate", 1, "Generations", lambda row: int(row[2]) == 3 and int(row[3]) == 3)
+
+        # Multiple generations for all scenarios of world 4
+        draw_plot(0,3,"simulate", 1, "Generations", lambda row: int(row[2]) == 3 and int(row[3]) == 1)
+        draw_plot(1,3,"simulate", 1, "Generations", lambda row: int(row[2]) == 3 and int(row[3]) == 2)
+        draw_plot(2,3,"simulate", 1, "Generations", lambda row: int(row[2]) == 3 and int(row[3]) == 3)
 
         # for requests in line 72
         #draw_scatter(0, 0, "war", 1, "Iterations", lambda row: int(row[2]) == 100 and int(row[3]) == 100)
         #draw_scatter(0, 1, "war", 1, "Iterations", lambda row: int(row[2]) == 50 and int(row[3]) == 50)
         #draw_scatter(0, 2, "war", 1, "Iterations", lambda row: int(row[2]) == 200 and int(row[3]) == 200)
 
-        draw_plot(0, 0, "war", 1, "Iterations", lambda row: int(row[2]) == 11 and int(row[3]) == 41)
-        draw_plot(1, 0, "war", 1, "Iterations", lambda row: int(row[2]) == 41 and int(row[3]) == 41)
-        draw_plot(2, 0, "war", 1, "Iterations", lambda row: int(row[2]) == 11 and int(row[3]) == 11)
+        #draw_plot(0, 0, "war", 1, "Iterations", lambda row: int(row[2]) == 11 and int(row[3]) == 41)
+        #draw_plot(1, 0, "war", 1, "Iterations", lambda row: int(row[2]) == 41 and int(row[3]) == 41)
+        #draw_plot(2, 0, "war", 1, "Iterations", lambda row: int(row[2]) == 11 and int(row[3]) == 11)
 
         #draw_plot(0, 0, "war", 1, "Iterations", lambda row: int(row[2]) == 100 and int(row[3]) == 100)
         #draw_plot(0, 1, "war", 3, "Army1 Size", lambda row: int(row[1]) == 1 and int(row[2]) == 1)
