42af7d07c91a5d4adb924eeb2565257e7ce6c70f
Diogo Santos <diogosantoss360@gmail.com>
2023-06-08 12:05:49 +0100

    Tried to fix compressimage but cant

--- worker/run.sh
+++ worker/run.sh
@@ -8,8 +8,7 @@ echo "Go to https://grupos.ist.utl.pt/meic-cnv/project/index-22-23.html to send 
 #   -javaagent:webserver/build/libs/webserver.jar=Metrics:pt.ulisboa.tecnico.cnv,javax.imageio:output \
 #   pt.ulisboa.tecnico.cnv.webserver.WebServer
 
-
-# java -cp webserver/build/libs/webserver.jar -javaagent:webserver/build/libs/webserver.jar=Metrics:pt.ulisboa.tecnico.cnv:output pt.ulisboa.tecnico.cnv.webserver.WebServer
+java -cp webserver/build/libs/webserver.jar -javaagent:webserver/build/libs/webserver.jar=Metrics:pt.ulisboa.tecnico.cnv:output pt.ulisboa.tecnico.cnv.webserver.WebServer
 
 # java \
 #   -cp webserver/build/libs/webserver.jar \
@@ -17,4 +16,4 @@ echo "Go to https://grupos.ist.utl.pt/meic-cnv/project/index-22-23.html to send 
 #   -javaagent:webserver/build/libs/webserver.jar=Metrics:pt.ulisboa.tecnico.cnv,javax.imageio:output \
 #   pt.ulisboa.tecnico.cnv.webserver.WebServer
 
-java -cp /webserver/target/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar -Xbootclasspath/a://javassist/target/JavassistWrapper-1.0-jar-with-dependencies.jar -javaagent:javassist/target/JavassistWrapper-1.0-jar-with-dependencies.jar=ICount:pt.ulisboa.tecnico.cnv,javax.imageio:output pt.ulisboa.tecnico.cnv.webserver.WebServer
+#java -cp webserver/target/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar -Xbootclasspath/a://javassist/target/JavassistWrapper-1.0.0-SNAPSHOT-jar-with-dependencies.jar -javaagent:javassist/target/JavassistWrapper-1.0.0-SNAPSHOT-jar-with-dependencies.jar=Metrics:pt.ulisboa.tecnico.cnv,javax.imageio:output pt.ulisboa.tecnico.cnv.webserver.WebServer
