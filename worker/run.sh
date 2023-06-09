8c00a0147d55293a46310098561689f2c6f807a6
Vasco Correia <vasco.correia@rnl.tecnico.ulisboa.pt>
2023-06-09 17:04:53 +0100

    run.sh finally wirks

--- worker/run.sh
+++ worker/run.sh
@@ -3,12 +3,12 @@
 echo "Accepting requests at http://localhost:8000"
 echo "Go to https://grupos.ist.utl.pt/meic-cnv/project/index-22-23.html to send requests"
 
-# java -cp webserver/build/libs/webserver.jar \
-#   -Xbootclasspath/a://javassist/build/libs/javassist.jar \
-#   -javaagent:webserver/build/libs/webserver.jar=Metrics:pt.ulisboa.tecnico.cnv,javax.imageio:output \
-#   pt.ulisboa.tecnico.cnv.webserver.WebServer
+java -cp webserver/build/libs/webserver.jar \
+  -Xbootclasspath/a:javassist/build/libs/javassist.jar \
+  -javaagent:javassist/build/libs/javassist.jar=Metrics:pt.ulisboa.tecnico.cnv,javax.imageio:output \
+  pt.ulisboa.tecnico.cnv.webserver.WebServer
 
-java -cp webserver/build/libs/webserver.jar -javaagent:webserver/build/libs/webserver.jar=Metrics:pt.ulisboa.tecnico.cnv:output pt.ulisboa.tecnico.cnv.webserver.WebServer
+# java -cp webserver/build/libs/webserver.jar -javaagent:webserver/build/libs/webserver.jar=Metrics:pt.ulisboa.tecnico.cnv:output pt.ulisboa.tecnico.cnv.webserver.WebServer
 
 # java \
 #   -cp webserver/build/libs/webserver.jar \
